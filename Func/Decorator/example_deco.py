83b22307695af09056ed9a1ae1e026c4e51c33c6
japablaza <japablaza@gmail.com>
2018-11-05 16:50:50 -0600

    Addin example_deco.py

---
+++ Func/Decorator/example_deco.py
@@ -0,0 +1,7 @@
+'''This is an example of function. Integers as input'''
+def sum_num(num):
+    return num + 1
+num = sum_num(2)
+print(num)
+
+'''The next function will take another function as an argument'''
