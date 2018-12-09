18f936ae41f590e0956a820d9b26d36a087a6811
japablaza <japablaza@gmail.com>
2018-12-08 19:42:07 -0600

    Adding parameters to clima route

--- virtual/pp.py
+++ virtual/pp.py
@@ -17,24 +17,12 @@ def testing():
     city_dict = city_func('Chicago', 'US')['id']
     return render_template('test_dict.html', **locals())
 
-@app.route('/weather')
-def weather():
-    all_data = [current_temp,
-                temp_min,
-                temp_max,
-                current_time_local,
-                city_search,
-                lon_search,
-                lat_search,
-                ]
-    return render_template('weather.html', ** locals())
-
-@app.route('/clima')
-def clima():
-    clima_dict = api_call_func_to_JSON('New York', 'US')
-    foto_link = type(photos_jason('Chicago'))
+@app.route('/clima/<city>')
+def clima(city=None):
+    clima_dict = api_call_func_to_JSON(city, 'US')
+    foto_link = random_photo(city)
     return render_template('clima.html', **locals())
 
-#
-# if __name__ == '__main__':
-#     app.run()
+
+if __name__ == '__main__':
+    app.run()
